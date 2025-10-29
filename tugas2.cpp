edbb4aef0907fec38265db28e7b22014f0fd3446
rachmad-hanafi <2500018044@webmail.uad.ac.id>
2025-10-30 00:50:15 +0700

    Update tugas2.cpp

--- tugas2.cpp
+++ tugas2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-	long detik;
+	long detik,totaldetik;
 	int hari , jam , menit;
 	
 	cout << "masukan angka >1 (dlm satuan detik) =";
@@ -11,12 +11,13 @@ int main(){
 	hari=detik/86400;
 	jam=(detik%86400)/3600;
 	menit=(detik%3600)/60;
-	detik=detik%60;
+	totaldetik=detik%60;
 	
 	cout << endl;
 	cout << "hasil konversi:" <<endl;
-	cout << hari << " "<<"hari" <<" "<< jam <<" "<< "jam"<< " " << menit <<" "<< "menit"<<" " << detik <<" "<< "detik";
+	cout << detik << " " << "detik" << " " << "=" << " "<< hari << " "<<"hari" <<" "<< jam <<" "<< "jam"<< " " << menit <<" "<< "menit"<<" " << totaldetik <<" "<< "detik";
 	
 	return 0;
 	
 }
+
